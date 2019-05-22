26f47fc57d6dc7ae44da11611bfd66f910405758
leoni <leonimega@hotmail.com>
2019-05-21 22:02:31 -0300

    update project with readme and decision tree code

--- Data Preprocessing/categorical_data.py
+++ Data Preprocessing/categorical_data.py
@@ -21,8 +21,14 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+# OneHotEncoder will split the column 0 into many other columns
+# this way the categories will be able to be encoded while being nominal values
+# (so in this case, a category won't have a bigger value than another)
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
+
 # Encoding the Dependent Variable
+# Another LabelEncoder is created since it has to fit the dependent variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
---
+++ Jupyter.md
@@ -0,0 +1,20 @@
+Install
+1. install conda from their website
+
+2. install jupyter notebooks and other libs
+`conda install jupyter notebook`
+or
+`conda install pandas scikit-learn=0.17 jupyter numpy`
+
+3. run jupyter
+`jupyter notebook`
+
+Anaconda commands:
+
+`conda env list` - Shows all the environments
+
+`conda list` - Lists all packages of the current environment
+
+`conda create -n myenv python=3.4` - Creates a new environment
+
+`conda activate myenv` or `source activate myenv` - Switches to the chosen environment
