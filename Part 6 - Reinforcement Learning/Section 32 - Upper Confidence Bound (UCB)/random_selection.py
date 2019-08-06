39bb86aa4c015705f006037c28a55a3b1ce3d678
leoni <leonimega@hotmail.com>
2019-08-06 08:01:27 -0300

    add Upper confidence bound

---
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/random_selection.py
@@ -0,0 +1,32 @@
+# Random Selection
+# this algorithm simulates what would happen if we picked random ads to be displayed
+# instead of using a ML algorithm to determine the best ad
+# the idea is to compare the results of this random selection/picking to the results
+# obtained by the algorithm 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# Implementing Random Selection
+import random
+N = 10000
+d = 10
+ads_selected = []
+total_reward = 0
+for n in range(0, N):
+    ad = random.randrange(d)
+    ads_selected.append(ad)
+    reward = dataset.values[n, ad]
+    total_reward = total_reward + reward
+
+# Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
